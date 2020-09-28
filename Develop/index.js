874ac975cf58fab00f92fe3df6dc65948c5ca2a4
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-27 22:45:39 -0500

    adding array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,40 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown)
+
+
+usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
 // array of questions for user
-const questions = [
+const questions = () => [
+    {
+        type: "input",
+        name: 'title',
+        message: 'What is your projects title?',
+    },
+    {
+        type: "input",
+        name: 'description',
+        message: 'Describe your project:',
+    },
+    {
+        type: "input",
+        name: 'installation',
+        message: 'Provide installation instructions:',
+    },
+    {
+        type: "input",
+        name: 'usage',
+        message: 'Provide information on the usage of your project:',
+    },
 
 ];
 
